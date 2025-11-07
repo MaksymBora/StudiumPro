996f70ae009e09c0af96d147879b24045bdaf66c
Maksym Borovichenko <maxboraod@gmail.com>
2025-11-07 11:45:25 +0100

    add productOffer

---
+++ src/components/Shop/ProductOffer.jsx
@@ -0,0 +1,44 @@
+import camImg from '../../assets/img/product-1.png';
+import watchImg from '../../assets/img/product-2.png';
+
+export function ProductOffer() {
+  return (
+    <div className="container-fluid bg-light py-5">
+      <div className="container">
+        <div className="row g-4">
+          <div className="col-lg-6 wow fadeInLeft" data-wow-delay="0.2s">
+            <a
+              href="#"
+              className="d-flex align-items-center justify-content-between border bg-white rounded p-4 text-decoration-none"
+            >
+              <div>
+                <p className="text-muted mb-3">Find The Best Camera for You!</p>
+                <h3 className="text-primary">Smart Camera</h3>
+                <h1 className="display-3 text-secondary mb-0">
+                  40% <span className="text-primary fw-normal">Off</span>
+                </h1>
+              </div>
+              <img src={camImg} className="img-fluid" alt="Smart Camera" style={{ maxHeight: '120px' }} />
+            </a>
+          </div>
+
+          <div className="col-lg-6 wow fadeInRight" data-wow-delay="0.3s">
+            <a
+              href="#"
+              className="d-flex align-items-center justify-content-between border bg-white rounded p-4 text-decoration-none"
+            >
+              <div>
+                <p className="text-muted mb-3">Find The Best Watches for You!</p>
+                <h3 className="text-primary">Smart Watch</h3>
+                <h1 className="display-3 text-secondary mb-0">
+                  20% <span className="text-primary fw-normal">Off</span>
+                </h1>
+              </div>
+              <img src={watchImg} className="img-fluid" alt="Smart Watch" style={{ maxHeight: '120px' }} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,12 @@
 import { Advantages } from '../components/Shop/Advantages.jsx';
+import { ProductOffer } from '../components/Shop/ProductOffer.jsx';
 import { ProductsWrapper } from '../components/Shop/ProductsWrapper/ProductsWrapper.jsx';
 
 export function Shop() {
   return (
     <div>
       <Advantages></Advantages>
+      <ProductOffer></ProductOffer>
       <ProductsWrapper></ProductsWrapper>
     </div>
   );
