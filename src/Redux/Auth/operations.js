627e20c0708064f49eec8c2d0e28b66a5153a132
Maksym Borovichenko <maxboraod@gmail.com>
2025-11-26 09:26:56 +0100

    add registration

--- src/Redux/Auth/operations.js
+++ src/Redux/Auth/operations.js
@@ -1,14 +1,30 @@
-import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 axios.defaults.baseURL = 'https://localhost:7086';
 
 export const loginUser = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
   try {
-    const response = await axios.post('/api/Auth/login', credentials);
-    return response.data;
-  } catch (error) {
-    const message = error.response?.data?.message || error.response?.data?.error || error.message || 'Login failed';
-    return thunkAPI.rejectWithValue(message);
+    const { data } = await axios.post('/api/Auth/login', credentials);
+    return data;
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response?.data || err.message);
+  }
+});
+
+export const registerUser = createAsyncThunk('auth/register', async (newUser, thunkAPI) => {
+  try {
+    const { data } = await axios.post('/api/Auth/register', newUser);
+
+    await thunkAPI.dispatch(
+      loginUser({
+        login: newUser.username,
+        password: newUser.password,
+      })
+    );
+
+    return data;
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response?.data || err.message);
   }
 });
